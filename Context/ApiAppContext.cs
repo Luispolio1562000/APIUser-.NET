d27cd7db2e5022a08f950a2a2a2ecc37d37ea19f
Luispolio1562000 <guspolio@outlook.es>
2024-10-11 17:05:24 -0600

    Se establecieron relaciones

--- Context/ApiAppContext.cs
+++ Context/ApiAppContext.cs
@@ -12,6 +12,7 @@ namespace APIDEMO_.Context
         /// </summary>
         
         public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> Roles { get; set; }
         public ApiAppContext(DbContextOptions<ApiAppContext> options) : base(options){ }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -19,14 +20,29 @@ namespace APIDEMO_.Context
             base.OnModelCreating(builder);
 
             List<User> usersInitData = new List<User>();
-            usersInitData.Add(new User{Name = "Luis",LastName = "Fernandez"});
+            usersInitData.Add(new User { Name = "Luis", LastName = "Fernandez" });
             usersInitData.Add(new User { Name = "Luis", LastName = "Fernandez" });
             builder.Entity<User>().ToTable("User").HasData(usersInitData);
             builder.Entity<User>().HasKey(t => t.UserId);
+
+
+
+            builder.Entity<UserRole>().ToTable("UserRole").HasKey(p => p.UserRoleId);
+            List<UserRole> userRoles = new List<UserRole>();
+            userRoles.Add(new UserRole() { Role = "Admin", UserId = usersInitData[0].UserId });
+            userRoles.Add(new UserRole() { Role = "User", UserId = usersInitData[0].UserId });
+            userRoles.Add(new UserRole() { Role = "Support", UserId = usersInitData[0].UserId });
+            userRoles.Add(new UserRole() { Role = "Admin", UserId = usersInitData[1].UserId });
+
+            builder.Entity<UserRole>().HasData(userRoles);
+
+
+            ///
+            /// Relacion uno a uno
+            builder.Entity<UserRole>().HasOne<User>("User").WithOne("UserID");
+
+
         }
-
-   
-
     }
 }
 
